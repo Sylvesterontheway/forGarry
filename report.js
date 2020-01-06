9ed783fac18dbf29d4cbaabf14d1e0422618bdc6
Sylvesterontheway <53208324+Sylvesterontheway@users.noreply.github.com>
2020-01-06 14:34:51 +0800

    Add files via upload

---
+++ report.js
@@ -0,0 +1,33 @@
+var select = '<option selected="selected" disabled="disabled">宝贝年龄</option>';
+for (var i = 4; i < 19; i++) {
+	select += '<option value="' + i + '">' + i + '岁</option>'
+}
+$('.age').html(select);
+$(document).ready(function() {
+	new WOW().init();
+	$('.cont1').fadeIn(1000);
+	setTimeout(function() {
+		$('.cont1').click(function() {
+			$(this).hide();
+			$('.cont2').fadeIn(1000);
+			setTimeout(function() {
+				$('.cont2').click(function() {
+					$(this).hide();
+					$('.cont3').fadeIn(1000);
+					setTimeout(function() {
+						$('.cont3').click(function() {
+							$(this).hide();
+							$('.cont4').fadeIn(1000);
+							setTimeout(function() {
+								$('.cont4').click(function() {
+									$(this).hide();
+									$('.cont5').fadeIn(1000);
+								});
+							}, 5900);
+						});
+					}, 3800);
+				});
+			}, 5900);
+		});
+	}, 4500);
+})
